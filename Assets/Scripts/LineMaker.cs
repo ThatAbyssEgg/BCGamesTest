0682745c939ae3e9f056e3e72f4e7edc9026bb86
That_Abyss_Egg <probablynotfaputa@gmail.com>
2024-04-21 15:34:14 +0300

    Extra portion of urgent bugfixes (followers behavior fixed)

--- Assets/Scripts/LineMaker.cs
+++ Assets/Scripts/LineMaker.cs
@@ -11,6 +11,7 @@ namespace Assets.Scripts
 	{
 		public GameObject LinePrefab;
 		public Transform Pattern;
+		public Transform PCs;
 
 		LineRenderer line;
 		Vector3 previousPosition;
@@ -37,7 +38,7 @@ namespace Assets.Scripts
 				}
 
 				var screenPosDepth = Input.mousePosition;
-				screenPosDepth.z = 5f;
+				screenPosDepth.z = 2f;
 				Vector3 currentPosition = Camera.main.ScreenToWorldPoint(screenPosDepth);
 
 				if (Vector3.Distance(currentPosition, previousPosition) > minimalDistance)
@@ -51,6 +52,30 @@ namespace Assets.Scripts
 			if (Input.GetMouseButtonUp(0))
 			{				
 				isNewDrawing = true;
+
+				// Distributing the followers by counting the points and dividing it by the amount of followers. The result is the number of the point that gets occupied by one follower.
+				// For example, for 15 points with 3 followers, points with indexes 0, 5 and 10 will be occupied
+
+				//float occupiedPointNumber = line.positionCount / (PCs.childCount - 1);
+
+				//if (occupiedPointNumber < 1)
+				//{
+
+				//}
+
+				// Converting the line points into actual positions & occupying them with followers
+
+				float drawingMin = -0.5f, drawingMax = 1.5f;
+				float planeMin = -5f, planeMax = 5f;
+
+				var ratio = (planeMax - planeMin) / (drawingMax - drawingMin);
+
+				for (int i = 1; i < PCs.childCount; i++)
+				{
+					var childPosition = PCs.GetChild(i).localPosition;
+					var drawingPoint = line.GetPosition((i - 1) % (PCs.childCount - 1));
+					PCs.GetChild(i).localPosition= new Vector3((drawingPoint.x - drawingMin) * ratio + planeMin, childPosition.y, (drawingPoint.y - 15 - drawingMin) * ratio + planeMin); // Temp solution
+				}
 			}
 
 		}
